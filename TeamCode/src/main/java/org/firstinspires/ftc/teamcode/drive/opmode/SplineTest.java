56fba6b062327236acf12c08491e58b0ed73ac5e
dinomaster186 <peterwetherell186@gmail.com>
2021-10-02 17:29:23 -0700

    5:29 pm 10/2/21 trying to make it do rversed using trajectory sequence runner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/SplineTest.java
@@ -30,6 +30,7 @@ public class SplineTest extends LinearOpMode {
                 .splineTo(new Vector2d(30, 30), 0)
                 .build();
         TrajectorySequence traj1 = drive.trajectorySequenceBuilder(traj.end(),true)
+                //.setReversed(true)
                 .splineTo(new Vector2d(0, 0), Math.toRadians(180))
                 .build();
 
