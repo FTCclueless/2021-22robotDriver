e535c3311b15314e6d36f56f46e188d1b4c2ff45
dinomaster186 <peterwetherell186@gmail.com>
2021-10-27 11:03:40 -0700

    Think that the VelocityKalmanFilterTuner is finished

--- Version 1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SetUp/VelocityKalmanFilterTuner.java
+++ Version 1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SetUp/VelocityKalmanFilterTuner.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.drive.SetUp;
 
+import android.util.Log;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -15,7 +17,7 @@ import java.util.ArrayList;
 public class VelocityKalmanFilterTuner extends LinearOpMode {
 
     public static double DISTANCE = 100;
-    public static double FULL_SPEED_DIST = 20;
+    public static double FULL_SPEED_DIST = 0;
     public static double POWER = 0.5;
 
     @Override
@@ -42,9 +44,33 @@ public class VelocityKalmanFilterTuner extends LinearOpMode {
             drive.pinMotorPowers(POWER,POWER,POWER,POWER);
         }
         drive.setMotorPowers(0,0,0,0);
+
+        double minSDev = -1;
+        double bestW = 0;
         for (int i = 0; i < 100; i ++){
             double w = ((double)i+1.0)/100.0;
-
+            ArrayList<Double> v = new ArrayList<Double>();
+            v.add(vel.get(0));
+            double sum = 0;
+            for (int j = 1; j < vel.size(); j ++){
+                v.add(v.get(v.size()-1)*(1.0-w) + vel.get(j)*w);
+                sum += v.get(v.size()-1);
+            }
+            double average = sum/v.size();
+            double error = 0;
+            for (int j = 0; j < v.size(); j ++){
+                error += Math.pow(v.get(j)-average,2);
+            }
+            double SDev = Math.sqrt(error/v.size());
+            Log.e(w + " ", SDev + " ");
+            if (minSDev == -1 || SDev < minSDev){
+                minSDev = SDev;
+                bestW = w;
+            }
         }
+        telemetry.addData("Best SDev ", minSDev);
+        telemetry.addData("best W ", bestW);
+        telemetry.update();
+        while (opModeIsActive()){ }
     }
 }
