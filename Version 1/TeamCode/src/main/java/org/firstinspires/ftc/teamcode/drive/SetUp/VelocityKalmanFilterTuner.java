18d3b428d3d01d35801a746a4cb6bfe8a33372df
dinomaster186 <peterwetherell186@gmail.com>
2021-10-27 17:55:15 -0700

    Fixed the VelocityKalmanFilterTuner to not only favor small values

--- Version 1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SetUp/VelocityKalmanFilterTuner.java
+++ Version 1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SetUp/VelocityKalmanFilterTuner.java
@@ -36,27 +36,26 @@ public class VelocityKalmanFilterTuner extends LinearOpMode {
         while (!isStopRequested() && gamepad1.y) {
             idle();
         }
+        double sum = 0;
         while (!isStopRequested() && drive.currentPose.getX() <= DISTANCE) {
             drive.update();
             if (drive.currentPose.getX() >= FULL_SPEED_DIST){
                 vel.add(drive.relCurrentVelocity.getX());
+                sum += drive.relCurrentVelocity.getX();
             }
             drive.pinMotorPowers(POWER,POWER,POWER,POWER);
         }
         drive.setMotorPowers(0,0,0,0);
-
+        double average = sum/vel.size();
         double minSDev = -1;
         double bestW = 0;
         for (int i = 0; i < 100; i ++){
             double w = ((double)i+1.0)/100.0;
             ArrayList<Double> v = new ArrayList<Double>();
             v.add(vel.get(0));
-            double sum = 0;
             for (int j = 1; j < vel.size(); j ++){
                 v.add(v.get(v.size()-1)*(1.0-w) + vel.get(j)*w);
-                sum += v.get(v.size()-1);
             }
-            double average = sum/v.size();
             double error = 0;
             for (int j = 0; j < v.size(); j ++){
                 error += Math.pow(v.get(j)-average,2);
