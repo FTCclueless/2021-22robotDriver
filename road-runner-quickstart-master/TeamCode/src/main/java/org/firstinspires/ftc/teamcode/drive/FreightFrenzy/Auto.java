f74427654a36d21e59a32755e95d77e94da72e31
dinomaster186 <peterwetherell186@gmail.com>
2021-11-18 22:03:31 -0800

    10:03 pm 11/18 updated auto

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FreightFrenzy/Auto.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FreightFrenzy/Auto.java
@@ -15,78 +15,82 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 public class Auto extends LinearOpMode {
     SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
     Pose2d startingPose = new Pose2d(12,66,0);
+    Long start;
     @Override
     public void runOpMode() throws InterruptedException {
-        //double y = 24 - endPoint.getY();
-        //double x = y / Math.tan(Math.toRadians(57.5));
-        //Pose2d depositPoint = new Pose2d(x,y);
+        //TODO: implement alliances
         TrajectorySequence[] intake = new TrajectorySequence[5];
         Pose2d endPoint = new Pose2d(12,64,0);
-        boolean robotFinished = false;
-        for (int i = 0; i < 5; i ++) {
+        int numIntakes = 7;
+        int numMinerals = 0;
+        for (int i = 0; i < numIntakes; i ++) {
             intake[i] = drive.trajectorySequenceBuilder(endPoint)
-                    .addTemporalMarker(() -> {
-                        drive.startIntake(false);
-                        if (robotFinished) {
-                            drive.startDeposit(endPoint, new Pose2d(12,24),16);
-                        }
-                    })
                     .splineTo(new Vector2d(36.5, 64), 0)
                     .splineTo(new Vector2d(40 + (i / 3) * 4,64),Math.toRadians((i % 3) * -15))
                     .build();
-                    //.splineToConstantHeading(new Vector2d(38, 63 - 3 * (i % 3)),0)
-                    //.splineToConstantHeading(new Vector2d(40 + (i / 3) * 4, 63 - (i % 3) * 3),0)
         }
+        int capNum = 2;
         waitForStart();
-        setUp();
-
-
-        long start = System.currentTimeMillis();
-        int numMinerals = 0;
-
-
-        while (numMinerals < 5 && System.currentTimeMillis() - start <= 22000){
-
-
-            drive.followTrajectorySequence(intake[numMinerals]);
-
-
-            double power = 0.3;
-            double startHeading = drive.currentPose.getHeading();
-            while(drive.intakeCase <= 2){
-                double turn = drive.currentPose.getHeading() - startHeading;
-                drive.pinMotorPowers(power+turn,power+turn,power-turn,power-turn);
-                drive.update();
-            }
-
-
-            TrajectorySequence deposit = drive.trajectorySequenceBuilder(drive.currentPose)
-                    .setReversed(true)
-                    .splineTo(new Vector2d(36.5,endPoint.getY()),Math.toRadians(180))
-                    .setReversed(true)
-                    .splineToConstantHeading(new Vector2d(endPoint.getX(),endPoint.getY()),Math.toRadians(180))
-                    .turn(0)
-                    .build();
-            drive.followTrajectorySequence(deposit);
-
-
-            drive.intakeCase = 0;
-            drive.lastIntakeCase = 0;
-            drive.deposit();
-
-
-            while (System.currentTimeMillis() - drive.depositTime <= 1500) { //drive.slidesCase <= 4
-                drive.update();
-            }
-
-
+        Vector2d pos = new Vector2d(26 + 8.83 * capNum,46.75);
+        double hed = 0;
+        if(capNum == 2){
+            pos = new Vector2d(36.2, 44.7);
+            hed = 0.35622;
+        }
+        TrajectorySequence grabCap = drive.trajectorySequenceBuilder(startingPose)
+                .splineTo(pos,hed)
+                .build();
+        TrajectorySequence goToStart = drive.trajectorySequenceBuilder(new Pose2d(pos.getX(),pos.getY(),hed))
+                .splineToConstantHeading(new Vector2d(endPoint.getX(),endPoint.getY()),endPoint.getHeading())
+                .build();
+        //The program begins
+        setUp(startingPose);
+        //TODO: Drop the arm
+        drive.startDeposit(new Pose2d(pos.getX(),pos.getY(),hed), new Pose2d(12,24),16); //TODO: Make the height vary on the capNum
+        drive.followTrajectorySequence(grabCap);
+        waitForDeposit();
+        //TODO: Lift up arm
+        drive.followTrajectorySequence(goToStart);
+        while (numMinerals < numIntakes && System.currentTimeMillis() - start <= 30000 - 3150 - 750){
+            drive.startIntake(false);
+            drive.startDeposit(endPoint, new Pose2d(12,24),16);
+            drive.followTrajectorySequence(intake[numMinerals]); //going into the wearhouse
+            intakeMineral(0.4,drive.currentPose.getHeading(),2000); // getting a mineral
+            drive.followTrajectorySequence(returnToScoring(endPoint)); //going to an area to drop off the mineral
+            waitForDeposit(); // deposit the block when first possible
             numMinerals ++;
         }
+        drive.followTrajectorySequence(intake[0]);
+        //waiting out the rest of the time in auto
         while (opModeIsActive()){
             drive.update();
         }
     }
-    public void setUp(){
+    public void waitForDeposit(){
+        drive.deposit();
+        while (drive.slidesCase <= 4 && opModeIsActive()) { //System.currentTimeMillis() - drive.depositTime <= 1500
+            drive.update();
+        }
+    }
+    public TrajectorySequence returnToScoring(Pose2d endPoint){
+        return drive.trajectorySequenceBuilder(drive.currentPose)
+                .setReversed(true)
+                .splineTo(new Vector2d(36.5,endPoint.getY()),Math.toRadians(180))
+                .setReversed(true)
+                .splineToConstantHeading(new Vector2d(endPoint.getX(),endPoint.getY()),Math.toRadians(180))
+                .turn(0)
+                .build();
+    }
+    public void intakeMineral(double power, double targetHeading, long maxTime){
+        long startingTime = System.currentTimeMillis();
+        while(drive.intakeCase <= 2 && System.currentTimeMillis()-startingTime <= maxTime && opModeIsActive()){
+            double turn = drive.currentPose.getHeading() - targetHeading;
+            drive.pinMotorPowers(power+turn,power+turn,power-turn,power-turn);
+            drive.update();
+        }
+    }
+    public void setUp(Pose2d startingPose){
+        start = System.currentTimeMillis();
         drive.intakeCase = 0;
         drive.lastIntakeCase = 0;
         drive.update();
