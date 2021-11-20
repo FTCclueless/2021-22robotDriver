01208440c2a751ac4851bbdeaaa2c2827c53647e
dinomaster186 <peterwetherell186@gmail.com>
2021-11-19 19:21:33 -0800

    7:21 pm 11/19 Changed drop servo time to the Micheal calculated time.

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -501,7 +501,7 @@ public class SampleMecanumDrive extends MecanumDrive {
         lastIntakeCase = intakeCase;
         int a = intakeCase;
         switch (a) {
-            case 1: if (System.currentTimeMillis() - intakeTime >= 500){intakeCase ++;} break;  // waiting for the servo to drop
+            case 1: if (System.currentTimeMillis() - intakeTime >= 360){intakeCase ++;} break;  // waiting for the servo to drop
             case 2: if (currentIntake == -1 && rightIntakeVal <= 300){intakeCase ++;} if (currentIntake == 1 && leftIntakeVal <= 300){intakeCase ++;} break;
             case 3: if (System.currentTimeMillis() - intakeTime >= 600 && !transferMineral){intakeCase ++;} break;  // waiting for the servo to go up && slides to be back
             case 4: if(Math.abs(turretHeading - Math.toRadians(intakeTurretInterfaceHeading)*currentIntake) <= Math.toRadians(1)){intakeCase ++;} break;//wait for the slides to be in the correct orientation
